68b750f775f48377aa8e4129b4c3546b6cabbf78
Antondstd <45855111+Antondstd@users.noreply.github.com>
2021-10-16 21:23:54 +0300

    Build: Lab2 for testing

---
+++ Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/DependInfo.cmake
@@ -0,0 +1,83 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Core/Startup/startup_stm32f407vgtx.s" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Core/Startup/startup_stm32f407vgtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Core/Src/gpio.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Core/Src/gpio.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Core/Src/hal_driver.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Core/Src/hal_driver.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Core/Src/main.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Core/Src/main.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Core/Src/stm32f4xx_hal_msp.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Core/Src/stm32f4xx_it.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Core/Src/stm32f4xx_it.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Core/Src/syscalls.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Core/Src/syscalls.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Core/Src/sysmem.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Core/Src/sysmem.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Core/Src/system_stm32f4xx.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Core/Src/system_stm32f4xx.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "D:/University/7_semester/Computer system design/Labs/Lab1/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "D:/University/7_semester/Computer system design/Labs/Lab1/cmake-build-debug/CMakeFiles/Lab1.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
