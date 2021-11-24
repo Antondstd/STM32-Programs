21e613af0106327a6ce9fd31001994b131d9c611
Anton Dankov <ad3std@gmail.com>
2021-11-24 11:28:47 +0300

    build: Lab2 reworked

--- Lab2/Core/Src/main.c
+++ Lab2/Core/Src/main.c
@@ -19,6 +19,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "i2c.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -26,6 +27,7 @@
 /* USER CODE BEGIN Includes */
 
 #include "hal_driver.h"
+#include <stdio.h>
 
 /* USER CODE END Includes */
 
@@ -58,6 +60,13 @@ void SystemClock_Config(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+#define KBRD_RD_ADDR 0xE3
+#define KBRD_WR_ADDR 0xE2
+#define ROW1 0x1E
+#define ROW2 0x3D
+#define ROW3 0x7B
+#define ROW4 0xF7
+
 enum MathOperation {
     ADDITION,
     SUBTRACTION,
@@ -67,9 +76,37 @@ enum MathOperation {
     NONE
 };
 
-int mode = 0;
-char ch[] = "F\n\r";
+int mode = 1;
+char ch[] = "F";
 char message[50] = "";
+char mode0[] = "Mode: Preriv \n\r";
+char mode1[] = "Mode: Bez Preriv \n\r";
+int canReceive = 1;
+short isFirst = 1;
+short counter = 0;
+short firstOperand = 0;
+short secondOperand = 0;
+enum MathOperation operation = NONE;
+
+typedef enum{
+	INPUT_PORT = 0x00, //Read byte XXXX XXXX
+	OUTPUT_PORT = 0x01, //Read/write byte 1111 1111
+	POLARITY_INVERSION = 0x02, //Read/write byte 0000 0000
+	CONFIG = 0x03 //Read/write byte 1111 1111
+}pca9538_regs_t;
+
+HAL_StatusTypeDef PCA9538_Read_Register(uint16_t addr, pca9538_regs_t reg, uint8_t* buf) {
+	return HAL_I2C_Mem_Read(&hi2c1, addr | 1, reg, 1, buf, 1, 100);
+}
+
+
+HAL_StatusTypeDef PCA9538_Write_Register(uint16_t addr, pca9538_regs_t reg, uint8_t* buf) {
+	return HAL_I2C_Mem_Write(&hi2c1, addr & 0xFFFE, reg, 1, buf, 1, 100);
+}
+
+HAL_StatusTypeDef PCA9538_Read_Inputs(uint16_t addr, uint8_t* buf) {
+	return PCA9538_Read_Register(addr, INPUT_PORT, buf);
+}
 
 short getNumber(char ch) {
     short asciNum = ch;
@@ -128,40 +165,233 @@ short makeMathOperation(enum MathOperation operation, short firstOperand, short 
 }
 
 void
-resetCalc(short *isFirst, short *counter, short *firstOperand, short *secondOperand, enum MathOperation *operation) {
-    *isFirst = 1;
-    *counter = 0;
-    *firstOperand = 0;
-    *secondOperand = 0;
-    *operation = NONE;
+resetCalc() {
+    isFirst = 1;
+    counter = 0;
+    firstOperand = 0;
+    secondOperand = 0;
+    operation = NONE;
 }
 
-char errorMessage[] = "ERROR\n\r";
+char errorMessage[] = "\n\rERROR\n\r";
+char errorMessageDiv[] = "\n\rCant divide by zero\n\r";
 
-void sendMessage(char* message, int size){
-	if (mode)
-		HAL_UART_Transmit(&huart6, (uint8_t*)message, size, 100);
-		else{
-		long time = getCurrentTime();
-		while(HAL_UART_Transmit_IT(&huart6, (uint8_t*)message, size) == HAL_BUSY && getCurrentTime()- time < 1000){
-
-		}
-		}
-
+void sendMessage(char *message, int size) {
+	size = strlen(message);
+    if (mode)
+        HAL_UART_Transmit(&huart6, (uint8_t *) message, size, 100);
+    else {
+//    	delay(1000);
+        long time = getCurrentTime();
+        HAL_StatusTypeDef isReaded1 = HAL_BUSY;
+        int curtime = getCurrentTime();
+        int counter = 0;
+        while (counter < 100000 && isReaded1 == HAL_BUSY) {
+        	isReaded1 = HAL_UART_Transmit_IT(&huart6, (uint8_t *) message, size);
+        	curtime = getCurrentTime();
+        	counter++;
+                }
+    }
 }
 
-void sendError(){
-	sendMessage(errorMessage,sizeof(errorMessage)-1);
-//	if (mode)
-//	HAL_UART_Transmit(&huart6, (uint8_t*)errorMessage, sizeof(errorMessage)-1, 100);
-//	else
-//		HAL_UART_Transmit_IT(&huart6, (uint8_t*)errorMessage, sizeof(errorMessage)-1);
+void sendError() {
+    sendMessage(errorMessage, sizeof(errorMessage) - 1);
 }
 
-void sendErrorAndReset(short *isFirst, short *counter, short *firstOperand, short *secondOperand, enum MathOperation *operation){
+void sendErrorAndReset() {
     sendError();
-    resetCalc(isFirst, counter, firstOperand, secondOperand, operation);
+    resetCalc();
 }
+
+void handle_calc() {
+    sendMessage(ch, 1);
+    enum MathOperation operationBuf = getMathOperation(ch[0]);
+    if (counter > 0) {
+        if (isFirst && operationBuf != NONE && operationBuf != EQUAL) {
+            operation = operationBuf;
+            isFirst = 0;
+            counter = 0;
+            return;
+        }
+        if (!isFirst && operationBuf != NONE && operationBuf == EQUAL) {
+            long testOverflow;
+            short res;
+            switch (operation) {
+                case ADDITION: {
+                    testOverflow = firstOperand + secondOperand;
+                    res = firstOperand + secondOperand;
+                    break;
+                }
+                case SUBTRACTION: {
+                    testOverflow = firstOperand - secondOperand;
+                    res = firstOperand - secondOperand;
+                    break;
+                }
+                case MULTIPLICATION: {
+                    testOverflow = firstOperand * secondOperand;
+                    res = firstOperand * secondOperand;
+                    break;
+                }
+                case DIVISION: {
+                    if (secondOperand == 0) {
+                        //sprintf(message, "\n\rCant divide by zero\n\r");
+                        sendMessage(errorMessageDiv, sizeof(errorMessageDiv) - 1);
+                        resetCalc();
+                        return;
+                    }
+                    testOverflow = firstOperand / secondOperand;
+                    res = firstOperand / secondOperand;
+                    break;
+                }
+            }
+            if (res == testOverflow){
+                sprintf(message, "%d\n\r", res);
+                sendMessage(message, sizeof(message) - 1);
+            }
+            else{
+                sendError();
+            }
+            resetCalc();
+            return;
+        }
+    }
+    if (operationBuf != NONE || counter > 4) {
+        sendErrorAndReset();
+        return;
+    }
+
+
+    short number = getNumber(ch[0]);
+    if (number != -1) {
+        if (isFirst) {
+            short numberBuf = checkNumbertoOverflow(firstOperand, number);
+            if (numberBuf == -1) {
+                sendErrorAndReset();
+                return;
+            }
+            firstOperand = numberBuf;
+        } else {
+            short numberBuf = checkNumbertoOverflow(secondOperand, number);
+            if (numberBuf == -1) {
+                sendErrorAndReset();
+                return;
+            }
+            secondOperand = numberBuf;
+        }
+        counter++;
+    } else {
+        sendErrorAndReset();
+    }
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+	canReceive=0;
+}
+
+HAL_StatusTypeDef Set_Keyboard( void ) {
+	HAL_StatusTypeDef ret = HAL_OK;
+	uint8_t buf;
+
+	buf=0x70;
+	ret = PCA9538_Write_Register(KBRD_WR_ADDR, CONFIG, &buf);
+	if( ret != HAL_OK ) {
+		//sendMessage("Error write config\n",1);
+		goto exit;
+	}
+
+	buf = 0;
+	ret = PCA9538_Write_Register(KBRD_WR_ADDR, OUTPUT_PORT, &buf);
+	if( ret != HAL_OK ) {
+		//sendMessage("Error write config\n",1);
+	}
+
+exit:
+	return ret;
+}
+
+uint8_t Check_Row( uint8_t  Nrow ) {
+	uint8_t Nkey = 0x00;
+	HAL_StatusTypeDef ret = HAL_OK;
+	uint8_t buf[4]={0,0,0,0};
+	uint8_t kbd_in;
+
+	ret = Set_Keyboard();
+//	if( ret != HAL_OK ) {
+//		sendMessage("Error write config\n",1);
+//	}
+
+	buf[0] = Nrow;
+
+	ret = PCA9538_Write_Register(KBRD_WR_ADDR, OUTPUT_PORT, buf);
+//	if( ret != HAL_OK ) {
+//		sendMessage("Error write output\n",1);
+//	}
+
+	buf[0] = 0;
+	ret = PCA9538_Read_Inputs(KBRD_RD_ADDR, buf);
+//	if( ret != HAL_OK ) {
+//		sendMessage("Read Error\n",1);
+//	}
+	kbd_in = buf[0] & 0x70;
+	Nkey = kbd_in;
+	if( kbd_in != 0x70) {
+		if( !(kbd_in & 0x10) ) {
+			switch (Nrow) {
+				case ROW1:
+					Nkey = 0x04;
+					break;
+				case ROW2:
+					Nkey = 0x04;
+					break;
+				case ROW3:
+					Nkey = 0x04;
+					break;
+				case ROW4:
+					Nkey = 0x04;
+					break;
+			}
+		}
+		if( !(kbd_in & 0x20) ) {
+			switch (Nrow) {
+				case ROW1:
+					Nkey = 0x02;
+					break;
+				case ROW2:
+					Nkey = 0x02;
+					break;
+				case ROW3:
+					Nkey = 0x02;
+					break;
+				case ROW4:
+					Nkey = 0x02;
+					break;
+			}
+		}
+		if( !(kbd_in & 0x40) ) {
+			switch (Nrow) {
+				case ROW1:
+					Nkey = 0x01;
+					break;
+				case ROW2:
+					Nkey = 0x01;
+					break;
+				case ROW3:
+					Nkey = 0x01;
+					break;
+				case ROW4:
+					Nkey = 0x01;
+					break;
+			}
+		}
+	}
+	else Nkey = 0x00;
+
+	return Nkey;
+}
+
+//void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
+//	canTransmit = 0;
+//}
 
 
 /* USER CODE END 0 */
@@ -173,15 +403,13 @@ void sendErrorAndReset(short *isFirst, short *counter, short *firstOperand, shor
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	char txt[] = "Hello world!\n\r";
-	char mode0[] = "Mode: Preriv \n\r";
-	char mode1[] = "Mode: Bez Preriv \n\r";
-	short isFirst = 1;
-	    short counter = 0;
-	    short firstOperand = 0;
-	    short secondOperand = 0;
-	    enum MathOperation operation = NONE;
-	    HAL_StatusTypeDef isReaded = HAL_BUSY;
+    HAL_StatusTypeDef isReaded;
+    int shortPressTime = 200;
+    int isBeingProceed = 0;
+    long startTimePressedButton = 0;
+    char chtest[] = "F\n\r";
+    uint8_t R = 0, C = 0, L = 0, Row[4] = {0xF7, 0x7B, 0x3D, 0x1E}, Key, OldKey, OLED_Keys[12] = {0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x30,0x30};
+
 
   /* USER CODE END 1 */
 
@@ -204,134 +432,39 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART6_UART_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+    while (1) {
 
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  if (!getButtonState()){
-		  if (mode == 1){
-			  mode = 0;
-			  sendMessage(mode0,sizeof(mode0)-1);
-			  delay(300);
-		  }
-		  else{
-			  mode = 1;
-			  sendMessage(mode1,sizeof(mode1)-1);
-			  delay(300);
-		  }
-	  }
-	  if (mode){
-	  isReaded = HAL_UART_Receive(&huart6, (uint8_t*)&ch, 1, 200);
-	  }
-	  else{
-		  isReaded = HAL_UART_Receive_IT(&huart6, (uint8_t*)&ch, 1);
-	  }
-//	  if (ch != 'F')
-	  if (isReaded == HAL_OK){
-//	sprintf(&ch,'%c\n\r',ch[0]);
-//		  if (mode){
-//	  HAL_UART_Transmit(&huart6, (uint8_t*)ch, sizeof(ch)-1, 100);
-//		  }
-//		  else{
-//			  HAL_UART_Transmit_IT(&huart6, (uint8_t*)ch, sizeof(ch)-1);
-//		  }
-		  sendMessage(ch,sizeof(ch)-1);
-	  isReaded == HAL_ERROR;
-	  enum MathOperation operationBuf = getMathOperation(ch[0]);
-	          if (counter > 0) {
-	              if (isFirst && operationBuf != NONE && operationBuf != EQUAL) {
-	                  operation = operationBuf;
-	                  isFirst = 0;
-	                  counter = 0;
-	                  continue;
-	              }
-	              if (!isFirst && operationBuf != NONE && operationBuf == EQUAL) {
-
-	                  switch (operation) {
-	                      case ADDITION: {
-	                          long testOverflow = firstOperand + secondOperand;
-	                          short res = firstOperand + secondOperand;
-	                          if (res == testOverflow)
-	                              sprintf(message,"Result: %d \n\r", res);
-	                          else{
-	                              sprintf(message,"ERROR\n");
-	                          }
-	                          break;
-	                      }
-	                      case SUBTRACTION:{
-	                          long testOverflow = firstOperand - secondOperand;
-	                          short res = firstOperand - secondOperand;
-	                          if (res == testOverflow)
-	                        	  sprintf(message,"Result: %d \n\r", res);
-	                          else
-	                        	  sprintf(message,"ERROR\n\r");
-	                          break;
-	                      }
-	                      case MULTIPLICATION: {
-	                          long testOverflow = firstOperand * secondOperand;
-	                          short res = firstOperand * secondOperand;
-	                          if (res == testOverflow)
-	                        	  sprintf(message,"Result: %d \n\r", res);
-	                          else
-	                        	  sprintf(message,"ERROR\n");
-	                          break;
-	                      }
-	                      case DIVISION: {
-	                          long testOverflow = firstOperand / secondOperand;
-	                          short res = firstOperand / secondOperand;
-	                          if (res == testOverflow)
-	                        	  sprintf(message,"Result: %d \n\r", res);
-	                          else
-	                        	  sprintf(message,"ERROR\n\r");
-	                          break;
-	                      }
-	                  }
-	                  sendMessage(message,sizeof(message)-1);
-//	                  HAL_UART_Transmit(&huart6, (uint8_t*)message, sizeof(message)-1, 100);
-	                  resetCalc(&isFirst, &counter, &firstOperand, &secondOperand, &operation);
-	                  continue;
-	              }
-	          }
-	          if (operationBuf != NONE) {
-	              sendErrorAndReset(&isFirst, &counter, &firstOperand, &secondOperand, &operation);
-	              continue;
-	          } else if (counter > 4) {
-	              sendErrorAndReset(&isFirst, &counter, &firstOperand, &secondOperand, &operation);
-	              continue;
-	          }
-
-
-	          short number = getNumber(ch[0]);
-	          if (number != -1) {
-	              if (isFirst) {
-	                  short numberBuf = checkNumbertoOverflow(firstOperand, number);
-	                  if (numberBuf == -1){
-	                      sendErrorAndReset(&isFirst, &counter, &firstOperand, &secondOperand, &operation);
-	                      continue;
-	                  }
-	                  firstOperand = numberBuf;
-	              } else {
-	                  short numberBuf = checkNumbertoOverflow(secondOperand, number);
-	                  if (numberBuf == -1){
-	                      sendErrorAndReset(&isFirst, &counter, &firstOperand, &secondOperand, &operation);
-	                  }
-	                  secondOperand = numberBuf;
-	              }
-	              counter++;
-	          }
-	          else{
-	        	  sendError();
-	          }
-	  }
-  }
+    	//sendMessage(chtest, sizeof(chtest)-1);
+    	for ( int i = 0; i < 4; i++ ) {
+    			while( !( R && C && L ) ) {
+    				OldKey = Key;
+    				Key = Check_Row( Row[i] );
+    				if ( Key == 0x01 && Key != OldKey) {
+    					sendMessage("Right pressed\n",1);
+    					R = 1;
+    				} else if ( Key == 0x02 && Key != OldKey) {
+    					sendMessage("Center pressed\n",1);
+    					C = 1;
+    				} else if ( Key == 0x04 && Key != OldKey) {
+    					sendMessage("Left pressed\n",1);
+    					L = 1;
+    				}
+    			}
+    			R = C = L = 0;
+    			HAL_Delay(25);
+    		}
+    	sendMessage("KB test complete\n",1);
+    }
   /* USER CODE END 3 */
 }
 
@@ -389,11 +522,10 @@ void SystemClock_Config(void)
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while (1) {
+    }
   /* USER CODE END Error_Handler_Debug */
 }
 
--- Lab2/Debug/Core/Src/main.d
+++ Lab2/Debug/Core/Src/main.d
@@ -22,11 +22,13 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
- ../Core/Inc/usart.h ../Core/Inc/main.h ../Core/Inc/gpio.h \
- ../Core/Src/hal_driver.h
+ ../Core/Inc/i2c.h ../Core/Inc/main.h ../Core/Inc/usart.h \
+ ../Core/Inc/gpio.h ../Core/Src/hal_driver.h
 
 ../Core/Inc/main.h:
 
@@ -76,15 +78,21 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
 
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
 
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
 
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
 
-../Core/Inc/usart.h:
+../Core/Inc/i2c.h:
 
 ../Core/Inc/main.h:
+
+../Core/Inc/usart.h:
 
 ../Core/Inc/gpio.h:
 
